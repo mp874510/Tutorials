40ce9d9a5476b678bbca8f08840b1ead95cd4785
Matt Porter <mp874510@ohio.edu>
2017-02-05 16:40:26 -0500

    Adding logic to calculate bmi

--- BMI/BMI/ViewController.swift
+++ BMI/BMI/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
 
+    var weight : Double?
+    var height : Double?
+    
+    var bmi : Double? {
+        get {
+            if(weight != nil && height != nil) {
+                return weight! / (height! * height!)
+            } else {
+                return nil
+            }
+        }
+    }
+    
     @IBOutlet weak var bmiLabel: UILabel!
     @IBOutlet weak var heightTextField: UITextField!
     @IBOutlet weak var weightTextField: UITextField!
@@ -28,6 +41,43 @@ class ViewController: UIViewController, UITextFieldDelegate {
         textField.resignFirstResponder()
         return true
     }
+    
+    func drawUI() {
+        if let bmi = self.bmi {
+            self.bmiLabel.text = String(format: "%4.1f",bmi)
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        func convert(number : String) -> Double? {
+            return NumberFormatter().number(from: number)?.doubleValue
+        }
+        
+        guard let text = textField.text else {
+            return
+        }
+        
+        switch(textField) {
+            case heightTextField:
+                self.height = convert(number: text)
+            case weightTextField:
+                self.weight = convert(number: text)
+            default:
+                print("Error updating UI")
+        }
+        
+        drawUI()
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 }
